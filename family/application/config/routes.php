4ff30900182b13f8216c3fe61e66cbab9bf212ae
mzaini30 <muhzaini30@gmail.com>
2019-11-07 15:19:02 +0800

    oke

--- family/application/config/routes.php
+++ family/application/config/routes.php
@@ -50,5 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'aplikasi';
+$route['tambah'] = 'aplikasi/tambah';
+$route['tambah/(:any)'] = 'aplikasi/tambah/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- family/application/controllers/Aplikasi.php
+++ family/application/controllers/Aplikasi.php
@@ -3,6 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Aplikasi extends MY_Controller {
 	public function index(){
-		$this->twig->display('aplikasi/beranda');
+		$data = $this->db->order_by('judul')->get('data')->result();
+		$this->twig->display('aplikasi/beranda', compact('data'));
+	}
+
+	public function tambah($info = ''){
+		if (!$_POST){
+			$this->twig->display('aplikasi/tambah', compact('info'));
+		} else {
+			$data = (object) $this->input->post();
+			$data->gambar = '';
+			$this->upload->initialize(array(
+				'upload_path' => './aset/gambar',
+				'allowed_types' => 'gif|jpg|png|jpeg',
+				'encrypt_name' => TRUE
+			));
+			if ($this->upload->do_upload('gambar')){
+				$data->gambar = $this->upload->data()['file_name'];
+			}
+			$this->db->insert('data', $data);
+			redirect(site_url() . 'tambah/berhasil');
+		}
 	}
 }
